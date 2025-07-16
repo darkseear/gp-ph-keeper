125d0aea1c2d8854b448570f678f1af5d1f3da36
DarkseearIS <darkseeara@yandex.ru>
2025-07-16 13:43:30 +0300

    уникальнуя соль

--- client/internal/crypto/crypto.go
+++ client/internal/crypto/crypto.go
@@ -7,6 +7,7 @@ import (
 	"crypto/sha256"
 	"errors"
 	"io"
+	"log"
 
 	"golang.org/x/crypto/pbkdf2"
 )
@@ -16,10 +17,19 @@ type CryptoService struct {
 	key []byte
 }
 
+const (
+	SALT_BYTE_SIZE    = 16
+	HASH_BYTE_SIZE    = 32
+	PBKDF2_ITERATIONS = 10000
+)
+
 // New -  экземпляр с созданием ключа.
 func New(masterPassword string) *CryptoService {
-	salt := []byte("salt")
-	key := pbkdf2.Key([]byte(masterPassword), salt, 10000, 32, sha256.New)
+	salt := make([]byte, SALT_BYTE_SIZE)
+	if _, err := rand.Read(salt); err != nil {
+		log.Fatalf("failed to generate salt: %v\n", err)
+	}
+	key := pbkdf2.Key([]byte(masterPassword), salt, PBKDF2_ITERATIONS, HASH_BYTE_SIZE, sha256.New)
 	return &CryptoService{key: key}
 }
 
