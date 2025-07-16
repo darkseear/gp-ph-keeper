acd82454ed77dea45f01fb03090aeb63e6e4a4d3
DarkseearIS <darkseeara@yandex.ru>
2025-07-16 10:47:22 +0300

    обработака шибок

--- client/cmd/main.go
+++ client/cmd/main.go
@@ -19,21 +19,30 @@ var (
 func main() {
 	if runtime.GOOS == "windows" && len(os.Args) == 1 {
 		// Получаем путь к текущему исполняемому файлу
-		exe, _ := os.Executable()
-		exePath, _ := filepath.Abs(exe)
+		exe, err := os.Executable()
+		if err != nil {
+			log.Fatalf("Error getting executable path: %v\n", err)
+		}
+
+		exePath, err := filepath.Abs(exe)
+		if err != nil {
+			log.Fatalf("Error getting absolute path: %v\n", err)
+		}
 
 		// Создаем команду для запуска cmd.exe
 		cmd := exec.Command("cmd.exe", "/C", "start", "cmd.exe", "/K", exePath, "--help")
 		cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: false}
 
 		// Запускаем
-		_ = cmd.Run()
+		if err = cmd.Run(); err != nil {
+			log.Fatalf("Error running cmd: %v\n", err)
+		}
+
 		return
 	}
 	rootCmd := comand.NewRootCmd(version, buildDate)
 	// Выполняем команду
 	if err := rootCmd.Execute(); err != nil {
-		log.Printf("Error: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("Error: %v\n", err)
 	}
 }
