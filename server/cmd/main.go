50037fb6a11e958fb7d99a234020863e40c9497e
DarkseearIS <darkseeara@yandex.ru>
2025-07-16 15:51:59 +0300

    отдельные компоненты

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -19,63 +19,111 @@ import (
 	"github.com/darkseear/gophkeeper/server/internal/storage"
 )
 
-// GRPCServer - структура для gRPC сервера.
+// Server - интерфейс для серверов.
+type Server interface {
+	Start(ctx context.Context) error
+	Stop(ctx context.Context) error
+}
+
+// GRPCServer - gRPC сервер.
 type GRPCServer struct {
-	Server *grpc.Server
+	server  *grpc.Server
+	address string
 }
 
-// App - основная структура приложения, содержащая серверы, хранилище и конфигурацию.
-type App struct {
-	GRPCServer *GRPCServer
-	Storage    *storage.Store
-	Cfg        *config.Config
+func NewGRPCServer(address string, stor *storage.Store, cfg *config.Config) *GRPCServer {
+	grpcSrv := grpc.NewServer()
+	nss := proto.NewGophkeeperGRPCServer(stor, cfg)
+	pb.RegisterGophkeeperServer(grpcSrv, nss)
+	return &GRPCServer{
+		server:  grpcSrv,
+		address: address,
+	}
 }
 
-// newApp - инициализирует приложение, настраивает логирование, хранилище и роутер.
-func newApp(ctx context.Context) (*App, error) {
+func (g *GRPCServer) Start(ctx context.Context) error {
+	listener, err := net.Listen("tcp", g.address)
+	if err != nil {
+		logger.Log.Error("Error starting gRPC server", zap.Error(err))
+		return err
+	}
+	logger.Log.Info("Starting GRPC server", zap.String("GRPCaddress", g.address))
+	go func() {
+		if err := g.server.Serve(listener); err != nil {
+			logger.Log.Error("gRPC server stopped with error", zap.Error(err))
+		}
+	}()
+	return nil
+}
+
+// Stop - остановка сервера .
+func (g *GRPCServer) Stop(ctx context.Context) error {
+	g.server.GracefulStop()
+	logger.Log.Info("gRPC server stopped")
+	return nil
+}
+
+// StorageComponent - компонент для работы с хранилищем.
+type StorageComponent struct {
+	store *storage.Store
+}
+
+// NewStorageComponent - экземпляр компонента сервера.
+func NewStorageComponent(cfg *config.Config) (*StorageComponent, error) {
+	stor, err := storage.NewStore(cfg)
+	if err != nil {
+		logger.Log.Error("Error store created")
+		return nil, err
+	}
+	return &StorageComponent{store: stor}, nil
+}
+
+// Close - звкрывает хранилище.
+func (s *StorageComponent) Close() error {
+	if err := s.store.Close(); err != nil {
+		logger.Log.Error("Error closing storage", zap.Error(err))
+		return err
+	}
+	logger.Log.Info("Storage closed")
+	return nil
+}
+
+// Application - агрегирует компоненты приложения.
+type Application struct {
+	servers []Server
+	storage *StorageComponent
+	cfg     *config.Config
+}
+
+// NewApplication - экземпляр агригатора приложения.
+func NewApplication(ctx context.Context) (*Application, error) {
 	cfg := config.New()
 	if err := logger.Initialize(cfg.LogLevel); err != nil {
 		return nil, err
 	}
 	defer logger.Log.Sync()
 
-	stor, err := storage.NewStore(cfg)
+	storageComp, err := NewStorageComponent(cfg)
 	if err != nil {
-		logger.Log.Error("Error store created")
 		return nil, err
 	}
 
-	// Настройка gRPC сервера
-	grpcSrv := grpc.NewServer()
-	nss := proto.NewGophkeeperGRPCServer(stor, cfg)
-	pb.RegisterGophkeeperServer(grpcSrv, nss)
+	grpcServer := NewGRPCServer(cfg.Address, storageComp.store, cfg)
 
-	return &App{
-		GRPCServer: &GRPCServer{
-			Server: grpcSrv,
-		},
-		Storage: stor,
-		Cfg:     cfg,
+	return &Application{
+		servers: []Server{grpcServer},
+		storage: storageComp,
+		cfg:     cfg,
 	}, nil
 }
 
-// Run - запускает приложение, инициализирует сервер и обрабатывает сигналы завершения.
-func (a *App) Run(ctx context.Context) {
-
-	// Запуск gRPC сервера
-	go func() {
-		listener, err := net.Listen("tcp", a.Cfg.Address)
-		if err != nil {
-			logger.Log.Error("Error starting gRPC server", zap.Error(err))
-			log.Fatalf("Error starting gRPC server: %v", err)
+// Run - запуск приложения.
+func (a *Application) Run(ctx context.Context) {
+	for _, srv := range a.servers {
+		if err := srv.Start(ctx); err != nil {
+			log.Fatalf("Failed to start server: %v", err)
 		}
-		logger.Log.Info("Starting GRPC server", zap.String("GRPCaddress", a.Cfg.Address))
-		err = a.GRPCServer.Server.Serve(listener)
-		if err != nil {
-			logger.Log.Error("Error starting gRPC server", zap.Error(err))
-			log.Fatalf("Error starting gRPC server: %v", err)
-		}
-	}()
+	}
 
 	<-ctx.Done()
 	logger.Log.Info("Received shutdown signal, shutting down server")
@@ -86,30 +134,23 @@ func (a *App) Run(ctx context.Context) {
 	} else {
 		logger.Log.Info("Server shutdown gracefully")
 	}
-
 }
 
-// Close - закрывает приложение, останавливает сервер и освобождает ресурсы.
-func (a *App) Close(ctx context.Context) error {
+// Close - закрывает приложение со всеми его компонентами.
+func (a *Application) Close(ctx context.Context) error {
 	var errs []error
 
-	// Останавливаем gRPC сервер
-	if a.GRPCServer != nil && a.GRPCServer.Server != nil {
-		a.GRPCServer.Server.GracefulStop()
-		logger.Log.Info("gRPC server stopped")
+	for _, srv := range a.servers {
+		if err := srv.Stop(ctx); err != nil {
+			errs = append(errs, err)
+		}
 	}
 
-	// Закрываем storage
-	if err := a.Storage.Close(); err != nil {
-		logger.Log.Error("Error closing storage", zap.Error(err))
+	if err := a.storage.Close(); err != nil {
 		errs = append(errs, err)
-	} else {
-		logger.Log.Info("Storage closed")
 	}
 
-	// Синхронизируем логгер
 	if err := logger.Log.Sync(); err != nil {
-		logger.Log.Error("Error syncing logger", zap.Error(err))
 		errs = append(errs, err)
 	}
 
@@ -124,7 +165,7 @@ func (a *App) Close(ctx context.Context) error {
 func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
 	defer stop()
-	app, err := newApp(ctx)
+	app, err := NewApplication(ctx)
 	if err != nil {
 		logger.Log.Error("Error create app", zap.Error(err))
 		log.Fatalf("Error app: %v", err)
